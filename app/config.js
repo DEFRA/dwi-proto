d4af4d2d6303b3e8a0ab2e41bf846845f5b2427e
Juan Fernandes <juan@fernandes.fyi>
2021-02-03 16:36:13 +0000

    Start work on section 5 - product formulation

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Chemicals For Use With Drinking Water',
+  serviceName: 'Drinking Water Inspectorate System',
 
   // Default port that prototype runs on
   port: '3000',
